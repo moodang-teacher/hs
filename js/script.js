df233b939683c78369d2bdd76fc8cb2b7536d96b
무당선생 <yssmi@naver.com>
2024-05-23 16:46:32 +0900

    모바일 용 메뉴 기본 동작 구현

--- js/script.js
+++ js/script.js
@@ -8,6 +8,22 @@ $(function () {
   const $btnMenu = $('.btn-menu');
   const duration = 300;
 
+  // 모바일
+  const $btnMmenu = $('.btn-m-menu');
+  const $mSubmenu = $('.m-submenu-wrap');
+  const $dim = $('.dim');
+  const $btnClose = $('.btn-close');
+
+  $btnMmenu.on('click', function () {
+    $mSubmenu.addClass('active');
+    $dim.fadeIn(duration);
+  });
+
+  $btnClose.on('click', function () {
+    $mSubmenu.removeClass('active');
+    $dim.fadeOut(duration);
+  });
+
   // 마우스가 메뉴에 들어오면(mouseenter)
   $menu.on('mouseenter', function () {
     const menuIdx = $(this).index();
@@ -40,21 +56,23 @@ $(function () {
     $banner.stop().fadeOut(duration);
   }
 
-  // 스크롤 이벤트
-  $window.on('scroll', function () {
-    // 얼마나 스크롤 되었는지 값을 구해서 저장
-    const scrollTop = $(this).scrollTop();
+  let scrollTop = $window.scrollTop();
+  const visualHeight = $('.visual').outerHeight();
+  setWhiteBackground();
 
-    // 비주얼 영역의 세로크기 저장
-    const visualHeight = $('.visual').outerHeight();
-    console.log(scrollTop, visualHeight);
-
-    // 두 값을 비교(스크롤값이 비주얼 영역의 세로보다 크다면=비주얼 영역을 지난다.)
+  function setWhiteBackground() {
     if (scrollTop >= visualHeight) {
       $header.addClass('w-bg');
     } else {
       $header.removeClass('w-bg');
     }
+  }
+
+  // 스크롤 이벤트
+  $window.on('scroll', function () {
+    // 얼마나 스크롤 되었는지 값을 구해서 저장
+    scrollTop = $(this).scrollTop();
+    setWhiteBackground();
   });
 
   // family site
